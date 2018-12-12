6f2aae0c061a5877fc1913bea929fda0fcc9ef7c
Zlash65 <zarrar65@gmail.com>
2018-12-13 03:52:53 +0530

    shopping contract - test case for whitelisting seller added

--- test/test_shopping.js
+++ test/test_shopping.js
@@ -27,7 +27,31 @@ contract("ShoppingSmartContract", function (accounts) {
 		}).then(assert.fail).catch(function(error) {
 			// catch error
 			assert(error.message.indexOf('Already added as seller') >= 0, "Already added as seller");
-		})
+		});
+	});
+
+	it("make seller whitelisted", function () {
+		return ShoppingSmartContract.deployed().then(function (instance) {
+			shopping = instance;
+
+			shopping.WhitelistAddress(accounts[1], {from: accounts[0]});
+
+			return shopping.sellers(accounts[1]);
+		}).then(function (seller) {
+			assert.equal(seller[1], true, "Seller whitelisted successfully.");
+
+			shopping.addAsSeller({from: accounts[2]}); // accounts[2] added as seller
+
+			return shopping.WhitelistAddress(accounts[1], {from: accounts[1]}); // try whitelisting from non admin account
+		}).then(assert.fail).catch(function(error) {
+			// catch onlyAdmin modifier's error
+			assert(error.message.indexOf('Admin access required') >= 0, "Admin access required");
+
+			return shopping.WhitelistAddress(accounts[3], {from: accounts[0]});
+		}).then(assert.fail).catch(function(error) {
+			// catch seller does not exist error
+			assert(error.message.indexOf('Seller with the given address does not exist.') >= 0, "Seller with the given address does not exist.");
+		});
 	});
 
 })
